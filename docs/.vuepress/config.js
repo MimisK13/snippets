6f51909efafe12a17e83579cd8b62dfb9077c548
MimisK <mimisk88@gmail.com>
2020-11-12 21:58:15 +0200

    Enable Plugins

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,34 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+	title: 'MimisK',
+	description: 'Just playing around',
+
+	base: '/snippets/',
+	
+	themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Guide', link: '/guide/' },
+			{ text: 'External', link: 'https://google.com' }
+		]
+	},
   
-  base: '/snippets'
+	plugins: [
+		[
+			'@vuepress/back-to-top'
+		],
+		[
+			'@vuepress/search', 
+			{
+				searchMaxSuggestions: 10
+			}
+		],
+		[
+			'@vuepress/last-updated',
+			{
+				dateOptions:{
+					hour12: false
+				}
+			}
+		]
+	]
 }
